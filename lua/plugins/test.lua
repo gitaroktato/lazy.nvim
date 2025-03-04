75fb983744545022417997be83e43e8f0fa3c2eb
gitaroktato <oresztesz_margaritisz@epam.com>
2025-03-04 08:12:38 +0100

    feat: Using debugpy with not just local breakpoints

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,5 +1,11 @@
 return {
-  {
-    "nvim-neotest/neotest",
+  "nvim-neotest/neotest",
+  opts = {
+    adapters = {
+      ["neotest-python"] = {
+        dap = { justMyCode = false },
+        pytest_discover_instances = true,
+      },
+    },
   },
 }
