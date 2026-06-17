3eb845b09640b54f7ff3543853a010232d7bf83a
gitaroktato <oresztesz_margaritisz@epam.com>
2026-06-17 13:57:36 +0200

    feat: Adding Java support to Nvim

---
+++ lua/plugins/neotest-java.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "rcasia/neotest-java",
+    ft = "java",
+    dependencies = {
+      -- "mfussenegger/nvim-jdtls",
+      "mfussenegger/nvim-dap", -- for debugging (optional)
+      "rcarriga/nvim-dap-ui", -- recommended
+      "theHamsta/nvim-dap-virtual-text", -- recommended
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-java")({
+            -- Optional configuration here
+          }),
+        },
+      })
+    end,
+  },
+}
