92a15417ddcc929ed26c354e2d8782e53ff6ccf4
gitaroktato <oresztesz_margaritisz@epam.com>
2026-09-14 09:41:42 +0200

    feat: Markdown table wrap not needed as render-markdown has it native

--- lua/plugins/markdown-table-wrap.lua
+++ lua/plugins/markdown-table-wrap.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "ice345/markdown-table-wrap.nvim",
-    ft = { "markdown", "quarto", "rmd" },
-    opts = {},
-  },
+  -- {
+  --   "ice345/markdown-table-wrap.nvim",
+  --   ft = { "markdown", "quarto", "rmd" },
+  --   opts = {},
+  -- },
 }
